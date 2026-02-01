8fb1ffc9f00efc596c1dc907f5e0cfca58034e38
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-01 15:00:45 -0300

    feat(js): update polynomial mirror with WASM delegation

--- src/js-mirror/poly.ts
+++ src/js-mirror/poly.ts
@@ -1,18 +1,30 @@
-export function polyEval(coeffs: number[] | Float64Array, x: number): number {
+import { getWasmProvider } from './wasm-provider';
+
+export function polyEval(coeffs: Float64Array | number[], x: number): number {
+    const wasm = getWasmProvider();
+    if (wasm && wasm.polyEval) return wasm.polyEval(coeffs, x);
+    
     let res = 0;
     for (let i = coeffs.length - 1; i >= 0; i--) res = res * x + coeffs[i];
     return res;
 }
 
-export function polyDerive(coeffs: number[] | Float64Array): number[] {
-    if (coeffs.length <= 1) return [0];
-    const res = [];
-    for (let i = 1; i < coeffs.length; i++) res.push(coeffs[i] * i);
-    return res;
+export function polyDerive(coeffs: Float64Array | number[]): Float64Array {
+    const wasm = getWasmProvider();
+    if (wasm && wasm.polyDerive) return wasm.polyDerive(coeffs);
+    
+    if (coeffs.length <= 1) return new Float64Array([0]);
+    const out = new Float64Array(coeffs.length - 1);
+    for (let i = 1; i < coeffs.length; i++) out[i - 1] = coeffs[i] * i;
+    return out;
 }
 
-export function polyIntegrate(coeffs: number[] | Float64Array, c: number): number[] {
-    const res = [c];
-    for (let i = 0; i < coeffs.length; i++) res.push(coeffs[i] / (i + 1));
-    return res;
+export function polyIntegrate(coeffs: Float64Array | number[], c: number): Float64Array {
+    const wasm = getWasmProvider();
+    if (wasm && wasm.polyIntegrate) return wasm.polyIntegrate(coeffs, c);
+    
+    const out = new Float64Array(coeffs.length + 1);
+    out[0] = c;
+    for (let i = 0; i < coeffs.length; i++) out[i + 1] = coeffs[i] / (i + 1);
+    return out;
 }
