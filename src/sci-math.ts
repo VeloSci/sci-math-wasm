e692e886509103bd313cb3a04c6f35772e9c9052
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-01 15:00:33 -0300

    feat(js): update SciMathJS interface to use WASM provider

--- src/sci-math.ts
+++ src/sci-math.ts
@@ -7,10 +7,12 @@ import * as poly from './js-mirror/poly';
 import * as calculus from './js-mirror/calculus';
 import { Complex } from './js-mirror/complex';
 import { SciEngineJS } from './js-mirror/engine';
+import { setWasmProvider } from './js-mirror/wasm-provider';
 
 /**
  * SciMathJS provides a pure TypeScript implementation of the SciMathWASM engine.
  * Users can choose between WASM and JS implementations depending on their needs.
+ * By using setWasmProvider, SciMathJS will delegate heavy calculations to WASM if available.
  */
 export const SciMathJS = {
   ...basic,
@@ -21,8 +23,9 @@ export const SciMathJS = {
   ...poly,
   ...calculus,
   Complex,
-  SciEngineJS
+  SciEngineJS,
+  setWasmProvider
 };
 
 // Export individual modules as well
-export { basic, stats, linalg, signal, fitting, poly, calculus, Complex, SciEngineJS };
+export { basic, stats, linalg, signal, fitting, poly, calculus, Complex, SciEngineJS, setWasmProvider };
