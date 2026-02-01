af4448e0203e35c60b044279baa5f8604f5662b0
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-01 15:00:21 -0300

    feat(api): expose unit conversion module components to WASM

--- src/units/mod.rs
+++ src/units/mod.rs
@@ -5,37 +5,37 @@
 use wasm_bindgen::prelude::*;
 
 /// Temperature conversion: Celsius to Fahrenheit.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = celsiusToFahrenheit)]
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     c * 1.8 + 32.0
 }
 
 /// Temperature conversion: Fahrenheit to Celsius.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = fahrenheitToCelsius)]
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     (f - 32.0) / 1.8
 }
 
 /// Temperature conversion: Celsius to Kelvin.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = celsiusToKelvin)]
 pub fn celsius_to_kelvin(c: f64) -> f64 {
     c + 273.15
 }
 
 /// Pressure conversion: Pascal to Bar.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = pascalToBar)]
 pub fn pascal_to_bar(pa: f64) -> f64 {
     pa / 100_000.0
 }
 
 /// Pressure conversion: Bar to Pascal.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = barToPascal)]
 pub fn bar_to_pascal(bar: f64) -> f64 {
     bar * 100_000.0
 }
 
 /// Distance conversion: Meters to Inches.
-#[wasm_bindgen]
+#[wasm_bindgen(js_name = metersToInches)]
 pub fn meters_to_inches(m: f64) -> f64 {
     m * 39.3701
 }
