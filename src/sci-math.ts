1c875c42abea94b6e85561639ad69747654b6a59
Jose Ivan <jigonzalez930209@gmail.com>
2026-01-31 23:20:49 -0300

    feat: unified sci-math entry point

---
+++ src/sci-math.ts
@@ -0,0 +1,28 @@
+import * as basic from './js-mirror/basic';
+import * as stats from './js-mirror/stats';
+import * as linalg from './js-mirror/linalg';
+import * as signal from './js-mirror/signal';
+import * as fitting from './js-mirror/fitting';
+import * as poly from './js-mirror/poly';
+import * as calculus from './js-mirror/calculus';
+import { Complex } from './js-mirror/complex';
+import { SciEngineJS } from './js-mirror/engine';
+
+/**
+ * SciMathJS provides a pure TypeScript implementation of the SciMathWASM engine.
+ * Users can choose between WASM and JS implementations depending on their needs.
+ */
+export const SciMathJS = {
+  ...basic,
+  ...stats,
+  ...linalg,
+  ...signal,
+  ...fitting,
+  ...poly,
+  ...calculus,
+  Complex,
+  SciEngineJS
+};
+
+// Export individual modules as well
+export { basic, stats, linalg, signal, fitting, poly, calculus, Complex, SciEngineJS };
