655f57884870ba2c805d8ff4fa2b739dca93d464
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-03 22:06:42 -0300

    feat: update engine analysis ops

--- src/engine_core/analysis.rs
+++ src/engine_core/analysis.rs
@@ -1,10 +1,23 @@
-use rayon::prelude::*;
+// use rayon::prelude::*;
 
-pub fn run_smooth_sg(n: usize, in_ptr: *const f64, out_ptr: *mut f64, window: usize) {
+pub fn run_smooth_sg(n: usize, i_ptr: *const f64, o_ptr: *mut f64, window: usize, degree: usize) {
     unsafe {
-        let in_slice = std::slice::from_raw_parts(in_ptr, n);
-        let out_slice = std::slice::from_raw_parts_mut(out_ptr, n);
-        crate::analysis::smooth_savitzky_golay(in_slice, window, out_slice);
+        crate::analysis::smooth_savitzky_golay(
+            std::slice::from_raw_parts(i_ptr, n),
+            window,
+            degree,
+            std::slice::from_raw_parts_mut(o_ptr, n),
+        );
+    }
+}
+
+pub fn run_detect_peaks(n: usize, i_ptr: *const f64, threshold: f64, prominence: f64) -> Vec<u32> {
+    unsafe {
+        crate::analysis::find_peaks(
+            std::slice::from_raw_parts(i_ptr, n),
+            threshold,
+            prominence
+        )
     }
 }
 
@@ -27,5 +40,25 @@ pub fn run_filter_butterworth(n: usize, i_ptr: *const f64, o_ptr: *mut f64, cuto
             cutoff, 
             fs
         );
+    }
+}
+pub fn run_remove_baseline(n: usize, i_ptr: *const f64, x_ptr: *const f64, o_ptr: *mut f64, order: usize, iters: usize) {
+    unsafe {
+        if iters <= 1 {
+            crate::analysis::remove_baseline(
+                std::slice::from_raw_parts(i_ptr, n),
+                std::slice::from_raw_parts(x_ptr, n),
+                order,
+                std::slice::from_raw_parts_mut(o_ptr, n),
+            );
+        } else {
+            crate::analysis::remove_baseline_iterative(
+                std::slice::from_raw_parts(i_ptr, n),
+                std::slice::from_raw_parts(x_ptr, n),
+                order,
+                iters,
+                std::slice::from_raw_parts_mut(o_ptr, n),
+            );
+        }
     }
 }
